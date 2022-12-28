0114bbbf227961cc42b7982395ba664640bce864
Damlrca <sir.damirca@yandex.ru>
2022-12-28 14:30:42 +0300

    2048

--- CSharpTest/Program.cs
+++ CSharpTest/Program.cs
@@ -8,136 +8,185 @@ namespace CSharpTest_Console_2048
 {
     internal class Program
     {
+        static int n = 4;
+        static Random rnd = new Random();
+        static int score = 0;
+
         static void Main(string[] args)
         {
-            int[,] m = new int[4,4];
+            int[,] m = new int[n, n];
 
-            New_random_number(ref m);
-            New_random_number(ref m);
+            Restart(ref m);
 
-            /*for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
-                    m[i, j] = Convert.ToInt32(Math.Pow(2, j + (i * 4)));
+            /*for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    m[i, j] = Convert.ToInt32(Math.Pow(2, j + (i * n)));
             m[0, 0] = 0;*/
 
-            Console.BackgroundColor = ConsoleColor.Gray;
             Console.CursorVisible = false;
 
+            Console.Title = "2048";
+            Console.WindowWidth = 3 * 2 + 9 * n;
+            Console.WindowHeight = 4 + 1 + 3 * n;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("   2048");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("   Текущий счёт:");
+            
             while (true)
             {
-                //Console.Clear();
-                Console.SetCursorPosition(0, 0);
-
-                Print(ref m);
-
+                Print(ref m, 3, 4);
                 Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(20 + 9 * (n - 3), 2);
+                Console.Write($"{score,10}");
+
                 Console.ForegroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition(0, 0);
                 ConsoleKeyInfo c = Console.ReadKey();
-                
-                if (c.Key == ConsoleKey.UpArrow)
+
+                switch(c.Key)
                 {
-                    Transpose(ref m);
-                    Do_move(ref m);
-                    Transpose(ref m);
+                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.W:
+                        Transpose(ref m);
+                        Do_move(ref m);
+                        Transpose(ref m);
+                        break;
+                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.S:
+                        Transpose(ref m);
+                        Reflect_vertically(ref m);
+                        Do_move(ref m);
+                        Reflect_vertically(ref m);
+                        Transpose(ref m);
+                        break;
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.D:
+                        Reflect_vertically(ref m);
+                        Do_move(ref m);
+                        Reflect_vertically(ref m);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                    case ConsoleKey.A:
+                        Do_move(ref m);
+                        break;
+                    case ConsoleKey.R:
+                        Restart(ref m);
+                        break;
+                    default:
+                        break;
                 }
-                else if (c.Key == ConsoleKey.DownArrow)
-                {
-                    Transpose(ref m);
-                    Reflect_vertically(ref m);
-                    Do_move(ref m);
-                    Reflect_vertically(ref m);
-                    Transpose(ref m);
-                }
-                else if (c.Key == ConsoleKey.RightArrow)
-                {
-                    Reflect_vertically(ref m);
-                    Do_move(ref m);
-                    Reflect_vertically(ref m);
-                }
-                else if (c.Key == ConsoleKey.LeftArrow)
-                {
-                    Do_move(ref m);
-                }
+
+                if (c.Key == ConsoleKey.Escape)
+                    break;
             }
         }
 
-        static Random rnd = new Random();
-
-        static void Print(ref int[,] m)
+        static void Restart(ref int[,] m)
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    m[i, j] = 0;
+            New_random_number(ref m);
+            New_random_number(ref m);
+            score = 0;
+        }
+
+        static void Print(ref int[,] m, int left, int top)
+        {
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    switch (m[i,j])
+                    SetColor(m[i, j]);
+                    for (int k = 0; k < 3; k++)
                     {
-                        case 0:
-                            Console.BackgroundColor = ConsoleColor.DarkGray;
-                            Console.ForegroundColor = ConsoleColor.DarkGray;
-                            break;
-                        case 2:
-                            Console.BackgroundColor = ConsoleColor.Gray;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case 4:
-                            Console.BackgroundColor = ConsoleColor.Cyan;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case 8:
-                            Console.BackgroundColor = ConsoleColor.DarkCyan;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case 16:
-                            Console.BackgroundColor = ConsoleColor.Blue;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case 32:
-                            Console.BackgroundColor = ConsoleColor.DarkBlue;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        case 64:
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        case 128:
-                            Console.BackgroundColor = ConsoleColor.DarkGreen;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        case 256:
-                            Console.BackgroundColor = ConsoleColor.DarkYellow;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case 512:
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            break;
-                        case 1024:
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        case 2048:
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        default:
-                            Console.BackgroundColor = ConsoleColor.Black;
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
+                        Console.SetCursorPosition(left + j * 9, top + i * 3 + k);
+                        Console.Write("         ");
                     }
-                    Console.Write($"{m[i, j],5}");
+                    Console.SetCursorPosition(left + j * 9, top + i * 3 + 1);
+                    Console.Write($"{m[i, j],6}");
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
             }
         }
+
+        static void SetColor(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case 2:
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case 4:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case 8:
+                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case 16:
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case 32:
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case 64:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case 128:
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case 256:
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case 512:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case 1024:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case 2048:
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
+
+        static int delta = 0;
 
         static void Do_move(ref int[,] m)
         {
+            delta = 0;
             if (Can_move(ref m))
             {
                 m = Move_left(ref m);
                 m = Merge_left(ref m);
                 m = Move_left(ref m);
                 New_random_number(ref m);
+                score += delta / 2;
             }
         }
 
@@ -148,8 +197,8 @@ namespace CSharpTest_Console_2048
             t = Merge_left(ref t);
             t = Move_left(ref t);
 
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
                     if (!t[i, j].Equals(m[i, j]))
                         return true;
 
@@ -159,16 +208,16 @@ namespace CSharpTest_Console_2048
         static void New_random_number(ref int[,] m)
         {
             int cnt = 0;
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
                     if (m[i, j] == 0)
                         cnt++;
             
             int t = rnd.Next(0, cnt) + 1;
             cnt = 0;
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < n; j++)
                 {
                     if (m[i, j] == 0)
                         cnt++;
@@ -191,11 +240,11 @@ namespace CSharpTest_Console_2048
 
         static int[,] Move_left(ref int[,] m)
         {
-            int[,] t = new int[4, 4];
-            int[] id = new int[4];
-            for (int i = 0; i < 4; i++)
+            int[,] t = new int[n, n];
+            int[] id = new int[n];
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < n; j++)
                 {
                     if (m[i, j] != 0)
                     {
@@ -210,14 +259,15 @@ namespace CSharpTest_Console_2048
         static int[,] Merge_left(ref int[,] m)
         {
             int[,] t = m;
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j + 1 < 4; j++)
+                for (int j = 0; j + 1 < n; j++)
                 {
                     if (t[i, j] == t[i, j + 1])
                     {
                         t[i, j] = t[i, j] + t[i, j];
                         t[i, j + 1] = 0;
+                        delta += t[i, j];
                     }
                 }
             }
@@ -226,18 +276,18 @@ namespace CSharpTest_Console_2048
 
         static void Reflect_vertically(ref int[,] m)
         {
-            int[,] t = new int[4, 4];
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
-                    t[i, j] = m[i, 3 - j];
+            int[,] t = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    t[i, j] = m[i, n - 1 - j];
             m = t;
         }
 
         static void Transpose(ref int[,] m)
         {
-            int[,] t = new int[4, 4];
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 4; j++)
+            int[,] t = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
                     t[i, j] = m[j, i];
             m = t;
         }
